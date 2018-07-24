54650f64f30e7a39b8c902dc760801cb8443f70d
Edith707 <edith.lopezhuerta@gmail.com>
2018-07-24 02:48:51 -0500

    first commit -m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskDetailComponent,
+    UsersComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
